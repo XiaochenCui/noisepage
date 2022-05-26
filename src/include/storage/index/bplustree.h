c5541ae3bea95d710c463d91c5c6274782f4eb6a
XiaochenCui <jcnlcxc.new@gmail.com>
2022-05-26 21:04:18 +0800

    update

--- src/include/storage/index/bplustree.h
+++ src/include/storage/index/bplustree.h
@@ -2,11 +2,54 @@
 
 #include <functional>
 
+#include "loggers/index_logger.h"
+
 namespace terrier::storage::index {
 
 template <typename KeyType, typename ValueType, typename KeyComparator = std::less<KeyType>,
           typename KeyEqualityChecker = std::equal_to<KeyType>, typename KeyHashFunc = std::hash<KeyType>,
           typename ValueEqualityChecker = std::equal_to<ValueType>>
-class BPlusTree {};
+class BPlusTree {
+ public:
+  BPlusTree() {
+    INDEX_LOG_INFO(
+        "B+ Tree Constructor called. "
+        "Setting up execution environment...");
+  }
+
+  ~BPlusTree() {
+    INDEX_LOG_INFO(
+        "B+ Tree Destructor called. "
+        "Cleaning up execution environment...");
+  }
+
+  void PrintContents() { INDEX_LOG_INFO("B+ Tree Contents:"); }
+
+  void Insert(const KeyType &key, const ValueType &value) {}
+
+  /*
+   * Iterator Interface
+   */
+  class ForwardIterator;
+
+  /*
+   * Begin() - Return an iterator pointing the first element in the tree
+   *
+   * If the tree is currently empty, then the iterator is both a begin
+   * iterator and an end iterator (i.e. both flags are set to true). This
+   * is a valid state.
+   */
+  ForwardIterator Begin() { return ForwardIterator{this}; }
+
+  /*
+   * Iterator Interface
+   */
+  class ForwardIterator {
+   public:
+    ForwardIterator(BPlusTree *tree) {}
+
+    bool IsEnd() { return false; }
+  };
+};
 
 }  // namespace terrier::storage::index
--- test/storage/bplustree_test.cpp
+++ test/storage/bplustree_test.cpp
@@ -1,3 +1,6 @@
+#include "storage/index/bplustree.h"
+
+#include "loggers/index_logger.h"
 #include "test_util/test_harness.h"
 
 namespace terrier::storage::index {
@@ -6,4 +9,45 @@ struct BPlusTreeTests : public TerrierTest {};
 
 // NOLINTNEXTLINE
 TEST_F(BPlusTreeTests, EmptyTest) { EXPECT_TRUE(true); }
+
+TEST_F(BPlusTreeTests, ForwardIterator) {
+  auto *const tree = new BPlusTree<int64_t, int64_t>();
+  tree->PrintContents();
+
+  const int key_num = 1024 * 1024;
+
+  // First insert from 0 to 1 million
+  for (int i = 0; i < key_num; i++) {
+    tree->Insert(i, i);
+  }
+
+  auto it = tree->Begin();
+
+  INDEX_LOG_INFO("it.IsEnd: %d", it.IsEnd());
+
+  // int64_t i = 0;
+  // while (!it.IsEnd()) {
+  //   EXPECT_EQ(it->first, it->second);
+  //   EXPECT_EQ(it->first, i);
+
+  //   i++;
+  //   it++;
+  // }
+
+  // EXPECT_EQ(i, key_num);
+
+  // auto it2 = tree->Begin(key_num - 1);
+  // auto it3 = it2;
+
+  // it2++;
+  // EXPECT_TRUE(it2.IsEnd());
+
+  // EXPECT_EQ(it3->first, key_num - 1);
+
+  // auto it4 = tree->Begin(key_num + 1);
+  // EXPECT_TRUE(it4.IsEnd());
+
+  delete tree;
+}
+
 }  // namespace terrier::storage::index
